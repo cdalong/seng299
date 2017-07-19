92c41508137649650cad1bb7f33f7ac2c6cf3a3e
Cam Long <cdalong@uvic.ca>
2017-07-19 12:16:54 -0700

    Fixed the merges of branches. fixed some changed method names and added the socket to the key of the user

--- A4/ShillBot/tests/test_mothership_basic.py
+++ A4/ShillBot/tests/test_mothership_basic.py
@@ -2,7 +2,47 @@
 import unittest
 
 from mothership.base import MothershipServer
+from workers.basic_worker import BasicUserParseWorker
 
 
 class TestMothershipBasic(unittest.TestCase):
     pass
+
+    #mothership starts to listen on the specified port
+    #receives data from workers
+
+
+    def testMothershipconnection(self):
+
+        mothership = MothershipServer()
+
+        mothership.run()
+
+        self.assertRaises(ConnectionRefusedError, mothership.run)
+
+    def workercanconnect(self):
+
+        mothership = MothershipServer()
+
+        worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
+
+        mothership.run()
+
+
+        try:
+            worker.run()
+        except:
+
+            self.fail("worker didn't work")
+
+    def receivesdata(self):
+
+        mothership = MothershipServer()
+        worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
+
+        mothership.run()
+        worker.run()
+
+        worker.send_to_mother(None,mothership)
+
+        self.assertRaises(ValueError, worker.send_to_mother(None,mothership)
