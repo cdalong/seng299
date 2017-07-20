0d905c612450db4434635f7ef644ddff84eda18a
AndrewBraun <ABandrewbraun@gmail.com>
2017-07-19 21:02:50 -0700

    Update Chatroom.py

--- Project/server/Chatroom.py
+++ Project/server/Chatroom.py
@@ -3,35 +3,34 @@ from GeneralChatroom import GeneralChatroom
 
 class Chatroom(GeneralChatroom):
 
-	def __init__(self, adminAddr, chatroomName):
+	def __init__(self, adminSocket, chatroomName):
 		GeneralChatroom.__init__(self)
 		self.name = chatroomName
-		self.admin = adminAddr
-		self.currentClients.append(adminAddr)
+		self.admin = adminSocket
+		self.currentClients.append(adminSocket)
 		self.blockedUsers = []
 		
-	def addUser(self, userAddr):
-		if userAddr not in self.blockedUsers:
-			GeneralChatroom.addUser(self, userAddr)
+	def addUser(self, userSocket):
+		if userSocket not in self.blockedUsers:
+			GeneralChatroom.addUser(self, userSocket)
 		
-	def removeUser(self, userAddr):
+	def removeUser(self, userSocket):
 		
-		GeneralChatroom.removeUser(self, userAddr)
+		GeneralChatroom.removeUser(self, userSocket)
 		
-		if userAddr == self.admin:
+		if userSocket == self.admin:
 			if len(self.currentClients) != 0:
-				self.admin = self.currentClients[0] #<-- Python 2.7
+				self.admin = self.currentClients[0]
 			else:
 				self.admin = None
 		
-	def blockUser(self, userAddr, blkrAddr):
+	def blockUser(self, userSocket, blkrSocket):
 		
-		if blkrAddr == self.admin:
-			if userAddr in self.currentClients:
-				self.blockedUsers[userAddr] = self.currentClients[userAddr]
+		if blkrSocket == self.admin and if userSocket in self.currentClients:
+			if userSocket in self.currentClients:
+				self.currentClients.remove(userSocket)
+			self.blockedUsers.append(userSocket)
 				
-	def unblockUser(self, userAddr, unblkrAddr):
-		
-		if unblkrAddr == self.admin:
-			if userAddr in self.blockedUsers:
-				del self.blockedUsers[userAddr]
+	def unblockUser(self, userSocket, unblkrSocket):
+		if unblkrSocket == self.admin and if userSocket in self.blockedUsers:
+			self.blockedUsers.remove(userSocket)
